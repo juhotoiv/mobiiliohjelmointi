d823412e948603b0ecf76fa2ab2e1d2c1e304aae
juhotoiv <juho.toivonen@myy.haaga-helia.fi>
2020-10-27 13:18:15 +0200

    Ex. 1: Calculator

---
+++ calculator/App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  const[firstNumber, setFirstNumber] = useState('');
+  const[secondNumber, setSecondNumber] = useState('');
+  const[result, setResult] = useState('');
+
+  const plusPressed = () => {
+    setResult(parseInt(firstNumber) + parseInt(secondNumber))
+  }
+
+  const minusPressed = () => {
+    setResult(parseInt(firstNumber) - parseInt(secondNumber))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{alignSelf: 'center'}}>Result: {result}</Text>
+      <TextInput
+          style={{borderColor: 'gray', borderWidth: 1}}
+          keyboardType={'number-pad'}
+          onChangeText={firstNumber => setFirstNumber(firstNumber)}
+          value={firstNumber}
+      />
+      <TextInput
+          style={{borderColor: 'gray', borderWidth: 1}}
+          keyboardType={'number-pad'}
+          onChangeText={secondNumber => setSecondNumber(secondNumber)}
+          value={secondNumber}
+      />
+      <StatusBar style="auto" />
+      <View style={{alignSelf: 'center', width: '50%', flexDirection: 'row', justifyContent: 'space-around'}}>
+        <Button title="+" onPress={plusPressed}/>
+        <Button title="-" onPress={minusPressed}/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+});
