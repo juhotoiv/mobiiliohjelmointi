9cc9c94fe60fd1c0e03ae701365d0da4fa145740
juhotoiv <juho.toivonen@myy.haaga-helia.fi>
2020-11-25 20:27:57 +0200

    Ex. 15: Text to speech

---
+++ text_to_speech/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech';
+
+export default function App() {
+  const [text, setText] = useState('')
+
+  const speak = () => {
+    Speech.speak(text);
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+          style={{width: 200, height: 50, borderColor: 'gray', borderWidth: 1, marginBottom: 10}}
+          onChangeText={text => setText(text)}
+          value={text}
+      />
+      <Button title="Press to hear text" onPress={() => speak()}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
